33ed02e23f06878c8f0fe5b35d478b103fca0c46
ehdin <Gabriel3749@users.noreply.github.com>
2026-04-24 00:04:24 -0300

    Botão rosa Cadastro

--- Inventario/frmCadastro.Designer.cs
+++ Inventario/frmCadastro.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
             this.button1.Location = new System.Drawing.Point(215, 90);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(381, 167);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // frmCadastro
             // 
